5008ddc3c28575a71da4843c860f47ed92d5d182
Sebastian Smerd <sebastiansmerd@Services-MBP.station>
2023-12-06 17:27:56 +0000

    Increase Book A Table Fonts

--- restr/booking/utils.py
+++ restr/booking/utils.py
@@ -1,4 +1,5 @@
 
 def print_parameters(form):
+    """ Print POST http parameters. Used for debugging. """
     for name, value in form.cleaned_data.items():
         print("{}: ({}) {}".format(name, type(value), value))
